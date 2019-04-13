500345c5eadb5a64e840f2939d98beb649c50c8e
Issei Horie <is2ei.horie@gmail.com>
2019-04-13 23:46:11 +0900

    HackerRank/New_Year_Chaos/cpp

---
+++ HackerRank/New_Year_Chaos/cpp/test.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+#./a.out<custom-input-00
+# ./a.out<custom-input-01
+
+# ./a.out<sample-input-00
+# ./a.out<sample-input-01
+
+# Expected Output
+# 966
+# ./a.out<input-02
+
+# Expected Output
+#-.-.-.-.-.-.-.-.-
+# 96110
+# Too chaotic
+# Too chaotic
+# 96319
+# 96323
+# 96058
+#-.-.-.-.-.-.-.-.-
+# ./a.out<input-06
+
+# Expected Output
+#-.-.-.-.-.-.-.-.-
+# 119814
+# 120175
+# 119810
+# 119827
+# Too chaotic
+#-.-.-.-.-.-.-.-.-
+./a.out<input-08
