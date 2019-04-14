ef8af3237e2d0d417ba385d4bab31fa274d8a397
Issei Horie <is2ei.horie@gmail.com>
2019-04-14 16:05:48 +0900

    HackerRank/Sorting-Comparator/cpp

---
+++ HackerRank/Sorting-Comparator/cpp/test.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# ./a.out<custom-input-00
+
+# ./a.out<sample-input-00
+# ./a.out<sample-input-01
+
+# Expected Output
+#-.-.-.-.-
+# ab 6
+# ade 5
+# c 5
+# bda 2
+# cab 2
+# db 2
+# a 1
+# cbb 1
+# bcc 0
+# dee 0
+#-.-.-.-.-
+# ./a.out<input-01
+
+
+./a.out<input-03
