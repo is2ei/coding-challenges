81cd87e0fd43c0ccbefdbf44ebf7b93c1792a62c
Issei Horie <is2ei.horie@gmail.com>
2019-04-15 22:42:13 +0900

    Refactor

--- HackerRank/Mark_and_Toys/cpp/test.sh
+++ HackerRank/Mark_and_Toys/cpp/test.sh
@@ -2,14 +2,14 @@
 
 g++ main.cpp
 
-OUTPUT_PATH=./output-00 ./a.out<sample-input-00
-cat output-00
+OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+cat sample-output-00
 
-OUTPUT_PATH=./output-01 ./a.out<sample-input-01
-cat output-01
+OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+cat sample-output-01
 
-OUTPUT_PATH=./output-02 ./a.out<sample-input-02
-cat output-02
+OUTPUT_PATH=./sample-output-02 ./a.out<sample-input-02
+cat sample-output-02
 
 # Expected Output
 # 19
