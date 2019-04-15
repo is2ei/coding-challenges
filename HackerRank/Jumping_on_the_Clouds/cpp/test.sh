c751ccd9973972ecd0bfb188c394c309984712ea
Issei Horie <is2ei.horie@gmail.com>
2019-04-15 20:05:46 +0900

    Refactor

--- HackerRank/Jumping_on_the_Clouds/cpp/test.sh
+++ HackerRank/Jumping_on_the_Clouds/cpp/test.sh
@@ -2,8 +2,8 @@
 
 g++ main.cpp
 
-OUTPUT_PATH=./output-01 ./a.out<sample-input-01
-cat output-01
+OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+cat sample-output-01
 
-OUTPUT_PATH=./output-02 ./a.out<sample-input-02
-cat output-02
+OUTPUT_PATH=./sample-output-02 ./a.out<sample-input-02
+cat sample-output-02
