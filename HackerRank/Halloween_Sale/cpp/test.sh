9449f0b9377cb1799dc309e8123130b3c41fa88d
Issei Horie <is2ei.horie@gmail.com>
2019-04-20 11:36:28 +0900

    HackerRank/Halloween_Sale/cpp

---
+++ HackerRank/Halloween_Sale/cpp/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+cat sample-output-00
+
+OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+cat sample-output-01
+
+OUTPUT_PATH=./output-02 ./a.out<input-02
+cat output-02
