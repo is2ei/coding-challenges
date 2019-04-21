a6bd0c6948871d5dcc3a2d0e54075dbccbac555f
Issei Horie <is2ei.horie@gmail.com>
2019-04-21 14:22:14 +0900

    HackerRank/Sherlock_and_Array/cpp

---
+++ HackerRank/Sherlock_and_Array/cpp/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+# cat sample-output-00
+
+# OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+# cat sample-output-01
+
+OUTPUT_PATH=./output-06 ./a.out<input-06
+cat output-06
