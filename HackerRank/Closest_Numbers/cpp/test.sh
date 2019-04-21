47c1c2827f5d9e4bfb8caa5e0cac79d7cdea0768
Issei Horie <is2ei.horie@gmail.com>
2019-04-21 11:59:02 +0900

    HackerRank/Closest_Numbers/cpp

---
+++ HackerRank/Closest_Numbers/cpp/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+OUTPUT_PATH=./custom-output-00 ./a.out<custom-input-00
+cat custom-output-00
+
+OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+cat sample-output-00
+
+OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+cat sample-output-01
+
+OUTPUT_PATH=./sample-output-02 ./a.out<sample-input-02
+cat sample-output-02
