457506d929a931cf9330a95c1170d3c52c5815cb
Issei Horie <is2ei.horie@gmail.com>
2019-04-25 23:28:25 +0900

    Refactor

--- HackerRank/Counting_Valleys/cpp/test.sh
+++ HackerRank/Counting_Valleys/cpp/test.sh
@@ -2,8 +2,8 @@
 
 g++ main.cpp
 
-OUTPUT_PATH=./sample-output ./a.out<sample-input
-cat sample-output
+OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+cat sample-output-00
 
 OUTPUT_PATH=./sample-output-02 ./a.out<sample-input-02
 cat sample-output-02
