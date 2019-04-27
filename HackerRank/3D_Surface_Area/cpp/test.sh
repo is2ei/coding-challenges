467cf3bcc0302f87dba20297f17f5383f0c46811
Issei Horie <is2ei.horie@gmail.com>
2019-04-27 23:34:56 +0900

    HackerRank/3D_Surface_Area/cpp

---
+++ HackerRank/3D_Surface_Area/cpp/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 6
+OUTPUT_PATH=./sample-output-00 ./a.out<sample-input-00
+cat sample-output-00
+
+# Expected Output
+# 60
+OUTPUT_PATH=./sample-output-01 ./a.out<sample-input-01
+cat sample-output-01
