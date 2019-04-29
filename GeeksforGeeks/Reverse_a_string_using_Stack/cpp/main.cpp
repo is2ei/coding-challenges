e6842cc4757ef502f03f1c7cdd0e7ee777184b0d
Issei Horie <is2ei.horie@gmail.com>
2019-04-30 01:12:51 +0900

    GeeksforGeeks/Reverse_a_string_using_Stack/cpp

---
+++ GeeksforGeeks/Reverse_a_string_using_Stack/cpp/main.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        reverse(str,len);
+        cout<<str;
+        cout<<endl;
+    }
+        return 0;
+}
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+void reverse(char *str, int len)
+{
+    stack<char> s;
+    for (int i = 0; i < len; i++) {
+        s.push(str[i]);
+    }
+    
+    
+    for (int i = 0; i < len; i++) {
+        str[i] = s.top();
+        s.pop();
+    }
+    
+}
