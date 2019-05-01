a4264ea4908391f7ef4af577b907e3ae3b968945
Issei Horie <is2ei.horie@gmail.com>
2019-05-02 00:05:32 +0900

    Updated templates

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -11,6 +11,22 @@ using namespace std;
 // string s;
 // getline(cin, s);
 
+namespace is2ei {
+
+    void rotateLeft(vector<int>& arr) {
+        if (arr.size() == 1) {
+            return;
+        }
+
+        int tmp = arr[0];
+        for (int i = 0; i < arr.size() - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.size() - 1] = tmp;
+    }
+
+}
+
 int main() {
 
     return 0;
