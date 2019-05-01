2fc41a40de1b7ea236ee583b0d02988bd5c1a5a3
Issei Horie <is2ei.horie@gmail.com>
2019-05-01 19:56:59 +0900

    GeeksforGeeks/Level_order_traversal/cpp

---
+++ GeeksforGeeks/Level_order_traversal/cpp/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+void levelOrder(struct Node* node);
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+void levelOrder(struct Node* node);
+/* Helper function to test mirror(). Given a binary
+   search tree, print out its data elements in
+   increasing sorted order.*/
+void inOrder(struct Node* node)
+{
+  if (node == NULL)
+    return;
+  inOrder(node->left);
+  printf("%d ", node->data);
+  inOrder(node->right);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *node = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (node == NULL)
+             node = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     levelOrder(node);
+     std::cout << endl;
+  }
+  return 0;
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+//You are required to complete this method
+void levelOrder(Node* node)
+{
+    queue<Node*> q;
+    q.push(node);
+    
+    while (!q.empty()) {
+        Node *tmp = q.front();
+        q.pop();
+        
+        if (tmp == nullptr) {
+            continue;
+        }
+        
+        std::cout << tmp->data << " ";
+        
+        q.push(tmp->left);
+        q.push(tmp->right);
+    }
+}
