fbe1142f377f4b721ec9937187de4e6288dd37a8
Issei Horie <is2ei.horie@gmail.com>
2019-05-01 23:48:05 +0900

    Refactor

--- templates/cpp/main.cpp
+++ templates/cpp/main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 // Read integer
 // int a;
 // cin >> a;
+// cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+// Read string
+// string s;
+// getline(cin, s);
 
 int main() {
+
     return 0;
 }
