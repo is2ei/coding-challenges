609078ac5ed1ecc0881d4d04b2b7f3d2ecca57f2
Issei Horie <is2ei.horie@gmail.com>
2019-05-01 21:10:19 +0900

    GeeksforGeeks/Node_at_a_given_index_in_linked_list/cpp

---
+++ GeeksforGeeks/Node_at_a_given_index_in_linked_list/cpp/main.cpp
@@ -0,0 +1,68 @@
+// C program to find n'th node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+ 
+/* Link list node */
+struct node
+{
+    int data;
+    struct node* next;
+};
+ 
+void push(struct node** head_ref, int new_data)
+{
+   
+    struct node* new_node =
+            (struct node*) malloc(sizeof(struct node));
+ 
+    new_node->data  = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref)    = new_node;
+}
+int GetNth(struct node* head, int index);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,k;
+    
+    cin>>T;
+    
+    while(T--){
+    struct node *head = NULL;
+        
+        cin>>n>>k;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            push(&head,l);
+        }
+   
+    printf("%d\n", GetNth(head, n-k));  
+    getchar();
+    }
+    return 0;
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Print he nth node in the linked list Node is defined as 
+struct node
+{
+    int data;
+    struct node* next;
+}; */
+// Should return data of node at given index. The function may
+//  assume that there are at least index+1 nodes in linked list
+int GetNth(struct node* head, int index){
+
+    while (index > 0) {
+        head = head->next;
+        index--;
+    }
+
+    return head->data;
+}
