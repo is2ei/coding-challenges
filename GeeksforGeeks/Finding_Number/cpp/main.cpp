4e57820cb73a120a8fba1e31b62b1dbe02096a04
Issei Horie <is2ei.horie@gmail.com>
2019-05-02 11:54:18 +0900

    GeeksforGeeks/Finding_Number/cpp

---
+++ GeeksforGeeks/Finding_Number/cpp/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(vector<int> arr, int x) {
+
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; i++) {
+
+        int n;
+        cin >> n;
+
+        vector<int> arr;
+        for (int j = 0; j < n; j++) {
+            int tmp;
+            cin >> tmp;
+            arr.push_back(tmp);
+        }
+
+        int x;
+        cin >> x;
+
+        int result = f(arr, x);
+        cout << result << endl;
+    }
+
+    return 0;
+}
