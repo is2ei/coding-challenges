dac34d36b471bf58b9422275753ec7ad6167372b
Issei Horie <is2ei.horie@gmail.com>
2019-05-06 00:00:41 +0900

    GeeksforGeeks/Mirror_Tree/cpp

---
+++ GeeksforGeeks/Mirror_Tree/cpp/main.cpp
@@ -0,0 +1,108 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+void mirror(struct Node* node);
+/* Helper function to test mirror(). Given a binary
+   search tree, print out its data elements in
+   increasing sorted order.*/
+void inOrder(struct Node* node)
+{
+  if (node == NULL)
+    return;
+  inOrder(node->left);
+  printf("%d ", node->data);
+  inOrder(node->right);
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  struct Node *child;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     mirror(root);
+     inOrder(root);
+     cout << endl;
+  }
+  return 0;
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//function Template for C++
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child /
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+/* Should convert tree to its mirror */
+void mirror(Node* node) 
+{
+    if (node == nullptr) {
+        return;
+    }
+    
+    queue<Node*> q;
+    q.push(node);
+    while (!q.empty()) {
+        Node* n = q.front();
+        q.pop();
+        
+        Node* tmp = n->left;
+        n->left = n->right;
+        n->right = tmp;
+        
+        if (n->left != nullptr) {
+            q.push(n->left);
+        }
+        
+        if (n->right != nullptr) {
+            q.push(n->right);
+        }
+    }
+}
