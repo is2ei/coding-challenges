32b2c3ffcc18ca82104fdd1f9f62d01cadcf64c9
Issei Horie <is2ei.horie@gmail.com>
2019-05-05 23:55:39 +0900

    GeeksforGeeks/Count_Leaves_in_Binary_Tree/cpp

---
+++ GeeksforGeeks/Count_Leaves_in_Binary_Tree/cpp/main.cpp
@@ -0,0 +1,109 @@
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+struct Node* newNode(int data)
+{
+  struct Node* node = new Node;
+  node->data = data;
+  node->left = NULL;
+  node->right = NULL;
+  return(node);
+}
+int countLeaves(struct Node* root);
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d\n",&n);
+     Node *root = NULL;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+      //  cout << n1 << " " << n2 << " " << (char)lr << endl;
+        if (m.find(n1) == m.end())
+        {
+           parent = newNode(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        Node *child = newNode(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << countLeaves(root) << endl;
+  }
+  return 0;
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+//User function Template for C++
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+/* Should return count of leaves. For example, return
+    value should be 2 for following tree.
+         10
+      /      \ 
+   20       30 */
+int countLeaves(Node* root)
+{
+    
+    int result = 0;
+    
+    if (root == nullptr) {
+        return result;
+    }
+    
+    queue<Node*> q;
+    q.push(root);
+    while (!q.empty()) {
+        Node* node = q.front();
+        q.pop();
+        
+        if (node->left == nullptr && node->right == nullptr) {
+            result++;
+        }
+        
+        if (node->left != nullptr) {
+            q.push(node->left);
+        }
+        
+        if (node->right != nullptr) {
+            q.push(node->right);
+        }
+    }
+    
+    return result;
+}
