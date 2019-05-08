3967ea62476ac0c4656c113d6995c543b2839901
Issei Horie <is2ei.horie@gmail.com>
2019-05-08 13:34:30 +0900

    GeeksforGeeks/Height_of_Heap/cpp

---
+++ GeeksforGeeks/Height_of_Heap/cpp/test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# ./a.out<custom-input-00
+
+# Expected Output
+# 2
+# 3
+./a.out<sample-input-00
+
+# Expected Output
+# 4
+./a.out<input-00
+
+# Expected Output
+# 2
+./a.out<input-01
