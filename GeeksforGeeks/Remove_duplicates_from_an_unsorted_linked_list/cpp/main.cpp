d766b82b0e13d1532eb39d08ef300044d814034b
Issei Horie <is2ei.horie@gmail.com>
2019-05-12 10:35:46 +0900

    GeeksforGeeks/Remove_duplicates_from_an_unsorted_linked_list/cpp

---
+++ GeeksforGeeks/Remove_duplicates_from_an_unsorted_linked_list/cpp/main.cpp
@@ -0,0 +1,83 @@
+//Code by Shubham Joshi 
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*
+The structure of linked list is the following
+struct Node
+{
+int data;
+Node* next;
+};
+*/
+Node *removeDuplicates(Node *root)
+{
+    Node* n = root;
+    
+    set<int> s;
+    s.insert(n->data);
+    while (n != nullptr && n->next != nullptr) {
+        s.insert(n->data);
+        if (s.find(n->next->data) != s.end()) {
+            n->next = n->next->next;
+            continue;
+        }
+        n = n->next;
+    }
+    
+    return root;
+}
