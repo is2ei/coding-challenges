733f88e76f5da9b981902e0602ad223e7dec0133
Issei Horie <is2ei.horie@gmail.com>
2019-05-17 01:48:16 +0900

    HackerRank/StringStream/cpp

---
+++ HackerRank/StringStream/cpp/main.cpp
@@ -0,0 +1,26 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> v;
+    string tmp;
+    while(std::getline(ss, tmp, ',')) {
+        v.push_back(std::stoi(tmp));
+    }
+    return v;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
