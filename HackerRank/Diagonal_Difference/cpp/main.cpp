b400454b0ab8aab0d67ff45b1b4b352451e0bdd0
Issei Horie <is2ei.horie@gmail.com>
2019-05-17 20:50:33 +0900

    HackerRank/Diagonal_Difference/cpp

---
+++ HackerRank/Diagonal_Difference/cpp/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the diagonalDifference function below.
+int diagonalDifference(vector<vector<int>> arr) {
+    int a = 0;
+    int b = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        a += arr[i][i];
+        b += arr[i][arr.size() - i - 1];
+    }
+    return std::abs(a - b);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<vector<int>> arr(n);
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
