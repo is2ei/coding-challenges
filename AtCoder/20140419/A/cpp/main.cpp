02445f736a8be717d42a0126db527db746a17d9b
Issei Horie <is2ei.horie@gmail.com>
2019-05-19 03:24:20 +0900

    AtCoder/20140419/A/cpp

---
+++ AtCoder/20140419/A/cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n) {
+    return n - 1;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int result = f(n);
+    cout << result << endl;
+
+    return 0;
+}
