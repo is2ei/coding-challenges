d44c2aa38e5f5aa78d9c9c7ad6a9c150c0572f28
Issei Horie <is2ei.horie@gmail.com>
2019-05-18 20:11:51 +0900

    AtCoder/20171028/A/cpp

---
+++ AtCoder/20171028/A/cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int r;
+    cin >> r;
+
+    int g;
+    cin >> g;
+
+    cout << g * 2 - r << endl;
+
+    return 0;
+}
---
+++ AtCoder/20171028/A/cpp/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 2032
+./a.out<sample-input-01
+
+# Expected Output
+# -4500
+./a.out<sample-input-02
