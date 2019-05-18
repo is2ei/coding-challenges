a69aeec11454ea987247caeaae5545dae9063f7d
Issei Horie <is2ei.horie@gmail.com>
2019-05-18 19:41:42 +0900

    AtCoder/20171213/B/cpp

---
+++ AtCoder/20171213/B/cpp/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    std::cout << "こんにちは" << std::endl;
+    std::cout << "AtCoder" << std::endl;
+
+    return 0;
+}
---
+++ AtCoder/20171213/B/cpp/test.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# こんにちは
+# AtCoder
+./a.out
