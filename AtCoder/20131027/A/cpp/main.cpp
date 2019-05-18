f1af935d524ef0ace5c03fe62589da7c5433f5b4
Issei Horie <is2ei.horie@gmail.com>
2019-05-19 02:42:46 +0900

    AtCoder/20131027/A/cpp

---
+++ AtCoder/20131027/A/cpp/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int x, y;
+    cin >> x >> y;
+
+    int result = std::max(x, y);
+
+    cout << result << endl;
+
+    return 0;
+}
---
+++ AtCoder/20131027/A/cpp/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 11
+./a.out<sample-input-01
+
+# Expected Output
+# 100000000
+./a.out<sample-input-02
