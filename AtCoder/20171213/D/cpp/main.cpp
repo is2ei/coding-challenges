7a64da2a0055479ec5a9639c884d37e9acad2f92
Issei Horie <is2ei.horie@gmail.com>
2019-05-18 19:50:11 +0900

    AtCoder/20171213/D/cpp

---
+++ AtCoder/20171213/D/cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    cout << 1 / 2.0 * 100 * (100 + 1) << endl;
+
+    return 0;
+}
---
+++ AtCoder/20171213/D/cpp/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 
+./a.out
