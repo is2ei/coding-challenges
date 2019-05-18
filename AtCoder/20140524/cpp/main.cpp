0ed40cd6f6ab5459e99453aaef3f7d0609316226
Issei Horie <is2ei.horie@gmail.com>
2019-05-19 03:35:44 +0900

    AtCoder/20140524/cpp

---
+++ AtCoder/20140524/cpp/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int f(int n) {
+    return n / 2 + n % 2;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int result = f(n);
+    cout << result << endl;
+
+    return 0;
+}
