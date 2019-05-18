698fcbf47799b7820f6d4dfac36bc9dbb751b1cc
Issei Horie <is2ei.horie@gmail.com>
2019-05-18 14:01:35 +0900

    Refactor

--- HackerRank/Linked_Lists-Detect_a_Cycle/cpp/main.cpp
+++ HackerRank/Linked_Lists-Detect_a_Cycle/cpp/main.cpp
@@ -1,14 +1,29 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as: 
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
 bool has_cycle(Node* head) {
     // Complete this function
     // Do not write the main method
-    int max_len = 100;
-    while(max_len-- && head != nullptr) {
-        head = head->next;        
+    if (head == nullptr || head->next == nullptr) {
+        return false;
     }
     
-    if (max_len > 0) {
-        return false;
-    } else {
-        return true;
+    Node* first = head;
+    Node* second = head->next;
+    while (second != nullptr && second->next != nullptr) {
+        if (first == second) {
+            return true;
+        }
+        first = first->next;
+        second = second->next->next;
     }
+    
+    return false;
 }
