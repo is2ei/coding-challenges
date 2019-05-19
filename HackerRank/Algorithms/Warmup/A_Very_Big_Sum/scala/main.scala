1e0d0e3745b6bcf079e4696d98b277cef954265b
Issei Horie <is2ei.horie@gmail.com>
2019-05-20 00:10:17 +0900

    HackerRank/Algorithms/Warmup/A_Very_Big_Sum/scala

---
+++ HackerRank/Algorithms/Warmup/A_Very_Big_Sum/scala/main.scala
@@ -0,0 +1,32 @@
+import java.io._
+import java.math._
+import java.security._
+import java.text._
+import java.util._
+import java.util.concurrent._
+import java.util.function._
+import java.util.regex._
+import java.util.stream._
+
+object Solution {
+
+    // Complete the aVeryBigSum function below.
+    def aVeryBigSum(ar: Array[Long]): Long = {
+        return ar.sum
+    }
+
+    def main(args: Array[String]) {
+        val stdin = scala.io.StdIn
+
+        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+        val arCount = stdin.readLine.trim.toInt
+
+        val ar = stdin.readLine.split(" ").map(_.trim.toLong)
+        val result = aVeryBigSum(ar)
+
+        printWriter.println(result)
+
+        printWriter.close()
+    }
+}
