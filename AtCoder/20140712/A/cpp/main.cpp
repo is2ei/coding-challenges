be2b49f9ff6eb7a7cd574c7fd0c1d82f8c1638a4
Issei Horie <is2ei.horie@gmail.com>
2019-05-19 09:52:21 +0900

    AtCoder/20140712/A/cpp

---
+++ AtCoder/20140712/A/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void f(int a, int b) {
+    cout << b << " " << a << endl;
+}
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    f(a, b);
+
+    return 0;
+}
---
+++ AtCoder/20140712/A/cpp/test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 2 1
+./a.out<sample-input-01
+
+# Expected Output
+# 5 5
+./a.out<sample-input-02
