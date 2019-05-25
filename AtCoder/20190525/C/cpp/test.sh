f727cf63ceb0ba47f08e21b701d48a31643523b1
Issei Horie <is2ei.horie@gmail.com>
2019-05-25 22:03:21 +0900

    AtCoder/20190525/C/cpp

---
+++ AtCoder/20190525/C/cpp/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+./a.out<custom-input-01
+
+# Expected Output
+# 2
+./a.out<sample-input-01
+
+# Expected Output
+# 1
+./a.out<sample-input-02
+
+# Expected Output
+# 100000
+./a.out<sample-input-03
