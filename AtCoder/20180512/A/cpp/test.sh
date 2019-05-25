34129541428b090a303c8490a069157624f65442
Issei Horie <is2ei.horie@gmail.com>
2019-05-25 18:17:22 +0900

    AtCoder/20180512/A/cpp

---
+++ AtCoder/20180512/A/cpp/test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# Yes
+./a.out<sample-input-01
+
+# Expected Output
+# No
+./a.out<sample-input-02
+
+# Expected Output
+# Yes
+./a.out<sample-input-03
+
+# Expected Output
+# Yes
+./a.out<sample-input-04
