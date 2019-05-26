8cff85300272b1ac966e0089c0df11ee5fba5144
Issei Horie <is2ei.horie@gmail.com>
2019-05-27 00:29:19 +0900

    HackerRank/Bit_Manipulation/Lonely_Integer/j

---
+++ HackerRank/Bit_Manipulation/Lonely_Integer/js/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the lonelyinteger function below.
+function lonelyinteger(a) {
+    var x = a[0];
+    for (var i = 1; i < a.length; i++) {
+        x = x ^ a[i];
+    }
+    return x;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const a = readLine().split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    let result = lonelyinteger(a);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
