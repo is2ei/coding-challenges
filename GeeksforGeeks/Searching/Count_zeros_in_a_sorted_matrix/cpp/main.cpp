f6a422a03c3e7608d3e4aa6a14ed965295ee8154
Issei Horie <is2ei.horie@gmail.com>
2019-06-01 00:26:52 +0900

    GeeksforGeeks/Searching/Count_zeros_in_a_sorted_matrix/cpp

---
+++ GeeksforGeeks/Searching/Count_zeros_in_a_sorted_matrix/cpp/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+#define MAX 1000
+int countZeroes(int A[MAX][MAX],int N);
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int A[MAX][MAX];
+		for(int i=0;i<n;i++)
+		for(int j=0;j<n;j++)
+		cin>>A[i][j];
+		cout<<countZeroes(A,n)<<endl;
+	}
+	return 0;
+}
+
+/*This is a function problem.You only need to complete the function given below*/
+/*you are required to complete this method*/
+int countZeroes(int A[MAX][MAX],int N)
+{
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (A[i][j] == 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
---
+++ GeeksforGeeks/Searching/Count_zeros_in_a_sorted_matrix/cpp/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+g++ main.cpp
+
+# Expected Output
+# 6
+./a.out<sample-input-00
