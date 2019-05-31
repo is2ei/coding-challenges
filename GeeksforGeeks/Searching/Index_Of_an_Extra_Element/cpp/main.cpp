f5530d8bdd6b5b60deb36c62d14b2ae2dff4d6a1
Issei Horie <is2ei.horie@gmail.com>
2019-06-01 01:00:19 +0900

    GeeksforGeeks/Searching/Index_Of_an_Extra_Element/cpp

---
+++ GeeksforGeeks/Searching/Index_Of_an_Extra_Element/cpp/main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findExtra(int a[],int b[],int n);
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n],b[n-1];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		for(int i=0;i<n-1;i++){
+			cin>>b[i];
+		}
+		cout<<findExtra(a,b,n)<<endl;
+	}
+}
+
+/*This is a function problem.You only need to complete the function given below*/
+/*Complete the function below*/
+int findExtra(int a[],int b[],int n)
+{
+    int sum = a[n - 1];
+    for (int i = 0; i < n - 1; i++) {
+        sum += a[i] - b[i];
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] == sum) {
+            return i;
+        }
+    }
+    return -1;
+}
