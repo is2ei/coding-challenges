7e72c6d8a52ba1d18579bd47ee7b1d8b7b5de128
Issei Horie <is2ei.horie@gmail.com>
2019-06-01 10:09:12 +0900

    AtCoder/20160507/A/cpp

---
+++ AtCoder/20160507/A/cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int a, int b, int c) {
+    if (a < b) {
+        return c / a;
+    }
+    return c / b;
+}
+
+int main() {
+
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int result = f(a, b, c);
+    cout << result << endl;
+
+    return 0;
+}
