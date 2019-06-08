a2ec546a6e5f88a752618876a31fc2817053b3fb
Issei Horie <is2ei.horie@gmail.com>
2019-06-08 11:04:46 +0900

    HackerRank/Data_Structures/Linked_Lists/Delete_a_Node/go

---
+++ HackerRank/Data_Structures/Linked_Lists/Delete_a_Node/go/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type SinglyLinkedListNode struct {
+	data int32
+	next *SinglyLinkedListNode
+}
+
+type SinglyLinkedList struct {
+	head *SinglyLinkedListNode
+	tail *SinglyLinkedListNode
+}
+
+func (singlyLinkedList *SinglyLinkedList) insertNodeIntoSinglyLinkedList(nodeData int32) {
+	node := &SinglyLinkedListNode{
+		next: nil,
+		data: nodeData,
+	}
+
+	if singlyLinkedList.head == nil {
+		singlyLinkedList.head = node
+	} else {
+		singlyLinkedList.tail.next = node
+	}
+
+	singlyLinkedList.tail = node
+}
+
+func printSinglyLinkedList(node *SinglyLinkedListNode, sep string, writer *bufio.Writer) {
+	for node != nil {
+		fmt.Fprintf(writer, "%d", node.data)
+
+		node = node.next
+
+		if node != nil {
+			fmt.Fprintf(writer, sep)
+		}
+	}
+}
+
+// Complete the deleteNode function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     data int32
+ *     next *SinglyLinkedListNode
+ * }
+ *
+ */
+func deleteNode(head *SinglyLinkedListNode, position int32) *SinglyLinkedListNode {
+	if position == 0 {
+		head = head.next
+	}
+	node := head
+	for position > 0 {
+		position--
+		if position == 0 {
+			node.next = node.next.next
+		}
+		node = node.next
+	}
+	return head
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	llistCount, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+
+	llist := SinglyLinkedList{}
+	for i := 0; i < int(llistCount); i++ {
+		llistItemTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+		checkError(err)
+		llistItem := int32(llistItemTemp)
+		llist.insertNodeIntoSinglyLinkedList(llistItem)
+	}
+
+	positionTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	position := int32(positionTemp)
+
+	llist1 := deleteNode(llist.head, position)
+
+	printSinglyLinkedList(llist1, " ", writer)
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
